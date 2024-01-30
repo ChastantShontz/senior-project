2616fc6e2699a0795a76247245613447f4c9dd5e
Chastant Shontz <shontzcp@clarkson.edu>
2024-01-30 16:15:35 -0500

    Commit

--- page.js
+++ page.js
@@ -1,5 +1,21 @@
 // Functions
 
+function changePage() {
+  if (sessionStorage.firstPage == "true") {
+    sessionStorage.firstPage = "false";
+  }
+  else if (sessionStorage.firstPage == "false") {
+    for (var i = 0; i < document.getElementsByClassName("navLink").length; i++) {
+      if (document.getElementsByClassName("navLink")[i].classList.contains("currentNavLink")) {
+        document.getElementsByClassName("navLink")[i].style.animation = "changePage .25s ease 0s 1 forwards";
+        setTimeout(() => (
+          document.getElementsByClassName("navLink")[i].style.animation = "none"
+        ), 250);
+      }
+    }
+  }
+}
+
 function downloadResume() {
   const day = new Date();
   document.getElementById("resumeDownloadFooterLink").download = document.getElementById("resumeDownloadFooterLink").download + "_" + day.getFullYear() + "-" + (((+day.getMonth() + 1) < 10) ? ("0" + (+day.getMonth() + 1)) : (+day.getMonth() + 1)) + "-" + ((day.getDate() < 10) ? ("0" + day.getDate()) : (day.getDate()));
