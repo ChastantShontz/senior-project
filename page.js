eeba487286e54594daa0c56de3f4f9fec934452d
Chastant Shontz <shontzcp@clarkson.edu>
2024-01-30 15:55:17 -0500

    Commit

--- page.js
+++ page.js
@@ -1,4 +1,6 @@
 // Functions
 
 function downloadResume() {
+  const day = new Date();
+  document.getElementById("resumeDownloadFooterLink").download = document.getElementById("resumeDownloadFooterLink").download + "_" + day.getFullYear() + "-" + (((+day.getMonth() + 1) < 10) ? ("0" + (+day.getMonth() + 1)) : (+day.getMonth() + 1)) + "-" + ((day.getDate() < 10) ? ("0" + day.getDate()) : (day.getDate()));
 }
