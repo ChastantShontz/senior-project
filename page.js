62e48a48f60ceea176c8536379f13756c9e4ed2a
Chastant Shontz <shontzcp@clarkson.edu>
2024-01-31 15:18:43 -0500

    Commit

--- page.js
+++ page.js
@@ -21,5 +21,5 @@ function changePage() {
 
 function downloadResume() {
   const day = new Date();
-  document.getElementById("resumeDownloadFooterLink").download = document.getElementById("resumeDownloadFooterLink").download + "_" + day.getFullYear() + "-" + (((+day.getMonth() + 1) < 10) ? ("0" + (+day.getMonth() + 1)) : (+day.getMonth() + 1)) + "-" + ((day.getDate() < 10) ? ("0" + day.getDate()) : (day.getDate()));
+  document.getElementById("resumeDownloadFooterLink").download = "Chastant_Shontz_Resume_" + day.getFullYear() + "-" + (((+day.getMonth() + 1) < 10) ? ("0" + (+day.getMonth() + 1)) : (+day.getMonth() + 1)) + "-" + ((day.getDate() < 10) ? ("0" + day.getDate()) : (day.getDate()));
 }
